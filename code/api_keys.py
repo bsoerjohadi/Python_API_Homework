8e3d61fb3fba846e28dab08e3c7b2a215dfa28c5
bsoerjohadi <bsoerjohadi@ucdavis.edu>
2020-08-19 22:29:16 -0700

    upload homework

--- code/api_keys.py
+++ code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "297fc46c2eaf0e982b3941514c496781"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyARwolX6G4UUCFwPJVlHG4vin7Gq8GTOuw"
