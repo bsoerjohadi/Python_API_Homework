d24d99834264f221fc8b96b3d474d2abd9a83be1
bsoerjohadi <bsoerjohadi@ucdavis.edu>
2020-08-19 22:36:22 -0700

    .

--- code/api_keys.py
+++ code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "297fc46c2eaf0e982b3941514c496781"
+weather_api_key = "YOUR API KEY"
 
 # Google API Key
-g_key = "AIzaSyARwolX6G4UUCFwPJVlHG4vin7Gq8GTOuw"
+g_key = "YOUR API KEY"
